7ccfb11833cc7cd1f8dce7bdd9e86ac32ad21955
MefodiyKo <125163900+MefodiyKo@users.noreply.github.com>
2026-06-28 21:48:09 +0500

    Update skyview_pro.js

--- js/skyview_pro.js
+++ js/skyview_pro.js
@@ -1,3 +1,12 @@
 /* Celestial Trainer - Sky View Pro */
 
 console.log("Sky View Pro loaded");
+
+/* PRO override */
+window.drawSkyView = function(){
+  drawSkyViewV3();
+};
+
+window.autoUpdateSky = function(){
+  drawSkyViewV3();
+};
