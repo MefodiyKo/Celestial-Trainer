91f82c9ea0fc3cce5b7ab3724e8e63432cd45b10
MefodiyKo <125163900+MefodiyKo@users.noreply.github.com>
2026-07-01 05:26:38 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-setNowUTC();
-populateObjectList();
-updateSightCorrections();
-autoUpdateSky();
-showStarPlanner();
-showPlanetFinder();
-showTheory();
-setupSkyViewControls();
+window.addEventListener("load", function(){
+
+  if(typeof setNowUTC==="function") setNowUTC();
+  if(typeof populateObjectList==="function") populateObjectList();
+  if(typeof updateSightCorrections==="function") updateSightCorrections();
+
+  if(typeof drawSkyView==="function") drawSkyView();
+  else if(typeof autoUpdateSky==="function") autoUpdateSky();
+
+  if(typeof showStarPlanner==="function") showStarPlanner();
+  if(typeof showPlanetFinder==="function") showPlanetFinder();
+  if(typeof showTheory==="function") showTheory();
+
+  if(typeof setupSkyViewControls==="function"){
+    setupSkyViewControls();
+  }
+
+});
