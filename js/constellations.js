c95f30c9078d532c6b1ed5f29e7bcf237fdc001f
MefodiyKo <125163900+MefodiyKo@users.noreply.github.com>
2026-07-01 07:50:29 +0500

    Update constellations.js

--- js/constellations.js
+++ js/constellations.js
@@ -1,210 +1,219 @@
-/* Celestial Trainer - Sky View Constellations */
+/* Celestial Trainer - Real Constellation Shapes */
 
 const CT_CONSTELLATIONS = {
-  "Ursa Major": [
-    ["Dubhe","Merak"],
-    ["Merak","Phecda"],
-    ["Phecda","Megrez"],
-    ["Megrez","Dubhe"],
-    ["Megrez","Alioth"],
-    ["Alioth","Mizar"],
-    ["Mizar","Alkaid"]
-  ],
+  "Cassiopeia":{
+    stars:{
+      Caph:{SHA:358.0,Dec:59.1},
+      Schedar:{SHA:350.0,Dec:56.5},
+      Navi:{SHA:347.0,Dec:60.7},
+      Ruchbah:{SHA:352.0,Dec:60.2},
+      Segin:{SHA:347.6,Dec:63.7}
+    },
+    lines:[
+      ["Caph","Schedar"],
+      ["Schedar","Navi"],
+      ["Navi","Ruchbah"],
+      ["Ruchbah","Segin"]
+    ]
+  },
 
-  "Ursa Minor": [
-    ["Polaris","Kochab"],
-    ["Kochab","Pherkad"]
-  ],
+  "Ursa Minor":{
+    stars:{
+      Polaris:{SHA:319.0,Dec:89.0},
+      Yildun:{SHA:175.0,Dec:86.6},
+      "Epsilon UMi":{SHA:124.0,Dec:82.0},
+      "Zeta UMi":{SHA:124.0,Dec:77.8},
+      Kochab:{SHA:137.0,Dec:74.0},
+      Pherkad:{SHA:130.0,Dec:71.8}
+    },
+    lines:[
+      ["Polaris","Yildun"],
+      ["Yildun","Epsilon UMi"],
+      ["Epsilon UMi","Zeta UMi"],
+      ["Zeta UMi","Kochab"],
+      ["Kochab","Pherkad"]
+    ]
+  },
 
-  "Cassiopeia": [
-    ["Caph","Schedar"],
-    ["Schedar","Navi"],
-    ["Navi","Ruchbah"],
-    ["Ruchbah","Segin"]
-  ],
+  "Cygnus":{
+    stars:{
+      Deneb:{SHA:50.0,Dec:45.0},
+      Sadr:{SHA:60.0,Dec:40.3},
+      Albireo:{SHA:69.0,Dec:27.9},
+      "Gienah Cygni":{SHA:56.0,Dec:33.9},
+      "Delta Cygni":{SHA:67.0,Dec:45.1}
+    },
+    lines:[
+      ["Deneb","Sadr"],
+      ["Sadr","Albireo"],
+      ["Sadr","Gienah Cygni"],
+      ["Sadr","Delta Cygni"]
+    ]
+  },
 
-  "Cygnus": [
-    ["Deneb","Sadr"],
-    ["Sadr","Albireo"],
-    ["Sadr","Gienah Cygni"],
-    ["Sadr","Delta Cygni"]
-  ],
+  "Lyra":{
+    stars:{
+      Vega:{SHA:81.0,Dec:39.0},
+      Sheliak:{SHA:77.0,Dec:33.4},
+      Sulafat:{SHA:76.0,Dec:32.7}
+    },
+    lines:[
+      ["Vega","Sheliak"],
+      ["Sheliak","Sulafat"],
+      ["Sulafat","Vega"]
+    ]
+  },
 
-  "Lyra": [
-    ["Vega","Sheliak"],
-    ["Sheliak","Sulafat"],
-    ["Sulafat","Vega"]
-  ],
+  "Aquila":{
+    stars:{
+      Altair:{SHA:62.0,Dec:9.0},
+      Tarazed:{SHA:74.0,Dec:10.6},
+      Alshain:{SHA:66.0,Dec:6.4}
+    },
+    lines:[
+      ["Altair","Tarazed"],
+      ["Altair","Alshain"]
+    ]
+  },
 
-  "Aquila": [
-    ["Altair","Tarazed"],
-    ["Altair","Alshain"]
-  ],
+  "Pegasus":{
+    stars:{
+      Markab:{SHA:14.0,Dec:15.0},
+      Scheat:{SHA:14.0,Dec:28.1},
+      Alpheratz:{SHA:358.0,Dec:29.0},
+      Algenib:{SHA:3.0,Dec:15.2},
+      Enif:{SHA:34.0,Dec:10.0}
+    },
+    lines:[
+      ["Markab","Scheat"],
+      ["Scheat","Alpheratz"],
+      ["Alpheratz","Algenib"],
+      ["Algenib","Markab"],
+      ["Markab","Enif"]
+    ]
+  },
 
-  "Orion": [
-    ["Betelgeuse","Bellatrix"],
-    ["Betelgeuse","Alnitak"],
-    ["Bellatrix","Mintaka"],
-    ["Alnitak","Alnilam"],
-    ["Alnilam","Mintaka"],
-    ["Alnitak","Saiph"],
-    ["Mintaka","Rigel"],
-    ["Saiph","Rigel"]
-  ],
+  "Andromeda":{
+    stars:{
+      Alpheratz:{SHA:358.0,Dec:29.0},
+      Mirach:{SHA:343.0,Dec:35.6},
+      Almach:{SHA:329.0,Dec:42.3}
+    },
+    lines:[
+      ["Alpheratz","Mirach"],
+      ["Mirach","Almach"]
+    ]
+  },
 
-  "Taurus": [
-    ["Aldebaran","Elnath"],
-    ["Aldebaran","Alcyone"]
-  ],
+  "Perseus":{
+    stars:{
+      Mirfak:{SHA:309.0,Dec:50.0},
+      Algol:{SHA:313.0,Dec:40.9},
+      Atik:{SHA:303.0,Dec:32.3},
+      Menkib:{SHA:296.0,Dec:35.8}
+    },
+    lines:[
+      ["Mirfak","Algol"],
+      ["Mirfak","Atik"],
+      ["Atik","Menkib"]
+    ]
+  },
 
-  "Gemini": [
-    ["Castor","Pollux"],
-    ["Pollux","Wasat"],
-    ["Wasat","Alhena"]
-  ],
+  "Taurus":{
+    stars:{
+      Aldebaran:{SHA:291.0,Dec:16.0},
+      Elnath:{SHA:279.0,Dec:29.0},
+      Alcyone:{SHA:304.0,Dec:24.1}
+    },
+    lines:[
+      ["Aldebaran","Elnath"],
+      ["Aldebaran","Alcyone"]
+    ]
+  },
 
-  "Canis Major": [
-    ["Sirius","Mirzam"],
-    ["Sirius","Adhara"],
-    ["Adhara","Wezen"],
-    ["Wezen","Aludra"]
-  ],
+  "Auriga":{
+    stars:{
+      Capella:{SHA:281.0,Dec:46.0},
+      Menkalinan:{SHA:280.0,Dec:44.9},
+      Elnath:{SHA:279.0,Dec:29.0}
+    },
+    lines:[
+      ["Capella","Menkalinan"],
+      ["Menkalinan","Elnath"],
+      ["Elnath","Capella"]
+    ]
+  },
 
-  "Leo": [
-    ["Regulus","Algieba"],
-    ["Algieba","Zosma"],
-    ["Zosma","Denebola"],
-    ["Regulus","Chertan"]
-  ],
+  "Orion":{
+    stars:{
+      Betelgeuse:{SHA:271.0,Dec:7.0},
+      Bellatrix:{SHA:279.0,Dec:6.0},
+      Mintaka:{SHA:276.0,Dec:-0.3},
+      Alnilam:{SHA:276.0,Dec:-1.2},
+      Alnitak:{SHA:275.0,Dec:-1.9},
+      Rigel:{SHA:282.0,Dec:-8.0},
+      Saiph:{SHA:275.0,Dec:-9.7}
+    },
+    lines:[
+      ["Betelgeuse","Bellatrix"],
+      ["Betelgeuse","Alnitak"],
+      ["Bellatrix","Mintaka"],
+      ["Alnitak","Alnilam"],
+      ["Alnilam","Mintaka"],
+      ["Alnitak","Saiph"],
+      ["Mintaka","Rigel"],
+      ["Saiph","Rigel"]
+    ]
+  },
 
-  "Boötes": [
-    ["Arcturus","Muphrid"],
-    ["Arcturus","Izar"],
-    ["Izar","Nekkar"],
-    ["Izar","Seginus"]
-  ],
+  "Canis Major":{
+    stars:{
+      Sirius:{SHA:259.0,Dec:-17.0},
+      Mirzam:{SHA:257.0,Dec:-17.9},
+      Adhara:{SHA:256.0,Dec:-29.0},
+      Wezen:{SHA:252.0,Dec:-26.4},
+      Aludra:{SHA:244.0,Dec:-29.3}
+    },
+    lines:[
+      ["Sirius","Mirzam"],
+      ["Sirius","Adhara"],
+      ["Adhara","Wezen"],
+      ["Wezen","Aludra"]
+    ]
+  },
 
-  "Virgo": [
-    ["Spica","Porrima"],
-    ["Porrima","Vindemiatrix"],
-    ["Porrima","Zavijava"]
-  ],
+  "Ursa Major":{
+    stars:{
+      Dubhe:{SHA:194.0,Dec:62.0},
+      Merak:{SHA:194.0,Dec:56.4},
+      Phecda:{SHA:184.0,Dec:53.7},
+      Megrez:{SHA:177.0,Dec:57.0},
+      Alioth:{SHA:167.0,Dec:56.0},
+      Mizar:{SHA:159.0,Dec:54.9},
+      Alkaid:{SHA:153.0,Dec:49.0}
+    },
+    lines:[
+      ["Dubhe","Merak"],
+      ["Merak","Phecda"],
+      ["Phecda","Megrez"],
+      ["Megrez","Dubhe"],
+      ["Megrez","Alioth"],
+      ["Alioth","Mizar"],
+      ["Mizar","Alkaid"]
+    ]
+  },
 
-  "Scorpius": [
-    ["Antares","Dschubba"],
-    ["Antares","Shaula"],
-    ["Shaula","Sargas"]
-  ],
-
-  "Sagittarius": [
-    ["Kaus Australis","Kaus Media"],
-    ["Kaus Media","Kaus Borealis"],
-    ["Kaus Australis","Nunki"]
-  ],
-
-  "Pegasus": [
-    ["Markab","Scheat"],
-    ["Scheat","Alpheratz"],
-    ["Alpheratz","Algenib"],
-    ["Algenib","Markab"],
-    ["Markab","Enif"]
-  ],
-
-  "Andromeda": [
-    ["Alpheratz","Mirach"],
-    ["Mirach","Almach"]
-  ],
-
-  "Perseus": [
-    ["Mirfak","Algol"],
-    ["Mirfak","Atik"],
-    ["Atik","Menkib"]
-  ],
-
-  "Crux": [
-    ["Acrux","Gacrux"],
-    ["Mimosa","Imai"]
-  ],
-
-  "Centaurus": [
-    ["Rigil Kentaurus","Hadar"],
-    ["Hadar","Menkent"]
-  ],
-
-  "Carina": [
-    ["Canopus","Miaplacidus"],
-    ["Miaplacidus","Avior"],
-    ["Avior","Aspidiske"]
-  ]
-};
-
-const CT_CONSTELLATION_STARS = {
-  Merak:{SHA:194.0,Dec:56.4},
-  Phecda:{SHA:184.0,Dec:53.7},
-  Megrez:{SHA:177.0,Dec:57.0},
-  Mizar:{SHA:159.0,Dec:54.9},
-  Pherkad:{SHA:130.0,Dec:71.8},
-
-  Caph:{SHA:358.0,Dec:59.1},
-  Navi:{SHA:347.0,Dec:60.7},
-  Ruchbah:{SHA:352.0,Dec:60.2},
-  Segin:{SHA:347.6,Dec:63.7},
-
-  Sadr:{SHA:60.0,Dec:40.3},
-  Albireo:{SHA:69.0,Dec:27.9},
-  "Gienah Cygni":{SHA:56.0,Dec:33.9},
-  "Delta Cygni":{SHA:67.0,Dec:45.1},
-
-  Sheliak:{SHA:77.0,Dec:33.4},
-  Sulafat:{SHA:76.0,Dec:32.7},
-
-  Tarazed:{SHA:74.0,Dec:10.6},
-  Alshain:{SHA:66.0,Dec:6.4},
-
-  Mintaka:{SHA:276.0,Dec:-0.3},
-  Alnitak:{SHA:275.0,Dec:-1.9},
-  Saiph:{SHA:275.0,Dec:-9.7},
-
-  Alcyone:{SHA:304.0,Dec:24.1},
-
-  Wasat:{SHA:247.0,Dec:21.5},
-  Alhena:{SHA:260.0,Dec:16.4},
-
-  Mirzam:{SHA:257.0,Dec:-17.9},
-  Wezen:{SHA:252.0,Dec:-26.4},
-  Aludra:{SHA:244.0,Dec:-29.3},
-
-  Algieba:{SHA:207.0,Dec:19.8},
-  Zosma:{SHA:191.0,Dec:20.5},
-  Chertan:{SHA:185.0,Dec:15.4},
-
-  Muphrid:{SHA:145.0,Dec:18.0},
-  Izar:{SHA:139.0,Dec:27.1},
-  Nekkar:{SHA:119.0,Dec:40.4},
-  Seginus:{SHA:128.0,Dec:38.3},
-
-  Porrima:{SHA:170.0,Dec:-1.4},
-  Vindemiatrix:{SHA:164.0,Dec:10.9},
-  Zavijava:{SHA:171.0,Dec:1.8},
-
-  Dschubba:{SHA:98.0,Dec:-22.6},
-  Sargas:{SHA:96.0,Dec:-42.9},
-
-  "Kaus Media":{SHA:84.0,Dec:-29.8},
-  "Kaus Borealis":{SHA:86.0,Dec:-25.4},
-
-  Scheat:{SHA:14.0,Dec:28.1},
-  Algenib:{SHA:3.0,Dec:15.2},
-
-  Mirach:{SHA:343.0,Dec:35.6},
-  Almach:{SHA:329.0,Dec:42.3},
-
-  Algol:{SHA:313.0,Dec:40.9},
-  Atik:{SHA:303.0,Dec:32.3},
-  Menkib:{SHA:296.0,Dec:35.8},
-
-  Mimosa:{SHA:170.0,Dec:-59.7},
-  Imai:{SHA:145.0,Dec:-59.7},
-
-  Aspidiske:{SHA:223.0,Dec:-59.5}
+  "Scorpius":{
+    stars:{
+      Antares:{SHA:113.0,Dec:-26.0},
+      Dschubba:{SHA:98.0,Dec:-22.6},
+      Shaula:{SHA:97.0,Dec:-37.0},
+      Sargas:{SHA:96.0,Dec:-42.9}
+    },
+    lines:[
+      ["Antares","Dschubba"],
+      ["Antares","Shaula"],
+      ["Shaula","Sargas"]
+    ]
+  }
 };
